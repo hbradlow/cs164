7afcb2d2f2afc3ec879deddb1cf91cf93bd61eda
Kevin Lindkvist <kevinlindkvist@berkeley.edu>
2012-10-02 01:00:00 -0700

    Testing script much easier to read output

--- testall.sh
+++ testall.sh
@@ -5,12 +5,13 @@ for f in $FILES
 do 
 	./apyc --phase=1 $f
 done
-echo
 echo "TESTING ERRORS" 
-echo
 FILES=tests/error/*.py
 for f in $FILES 
 do
-echo "Compiling error: $f"
-./apyc --phase=1 $f
+./apyc --phase=1 $f &> /dev/null
+rc=$?
+if [[ $rc != 1 ]] ; then
+        echo "Did not fail on $f"
+fi
 done
