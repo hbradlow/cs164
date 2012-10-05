8b4103a7d5af97e6b96355e2f0afb4fea8b82241
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-05 15:48:21 -0700

    Fixed dictionaries

--- tests/correct/dict_display.py
+++ tests/correct/dict_display.py
@@ -1,8 +1,9 @@
 a = {"hello":1,"goodbye":3}
 print a
-b = {"dict":a,"list":[1,2,3],"tuple":(4,5,6,),"int":9,"string":"this"}
+b = {"list":[1,2,3],"tuple":(4,5,6,),"int":9,"string":"this"}
 print b
 def foo():
     print "FUNCTION"
+    return True
 c = {"func":foo}
-print c["func"]
+print c["func"]()
