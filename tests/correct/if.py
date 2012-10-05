7a49509a4ce8277a9c9fba02cde931b74d8ba959
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-05 01:00:05 -0700

    Added tests

---
+++ tests/correct/if.py
@@ -0,0 +1,31 @@
+def test1(a,b):
+    if a==b:
+        print 1
+    elif a==4:
+        print 2
+    else:
+        print 3
+test1(1,1)
+test1(1,2)
+test1(4,3)
+test1(4,4)
+
+def test2(a,b):
+    if a==b:
+        print 1
+    elif a==4:
+        print 2
+test2(1,1)
+test2(1,2)
+test2(4,3)
+test2(4,4)
+
+def test3(a,b):
+    if a==b:
+        print 1
+    else:
+        print 2
+test3(1,1)
+test3(1,2)
+test3(4,3)
+test3(4,4)
