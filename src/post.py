8d9677d959ecf0782011e44e93565f2baa2d5ef7
Will Skinner <meaningoflifeis42@gmail.com>
2012-10-05 18:30:07 -0700

    fixed the bug in post

--- src/post.py
+++ src/post.py
@@ -1,13 +1,21 @@
 import re
-patt = re.compile(r'\(list_display(\s\d+((\n\s+\(id\s\d+\s\w+\))*)\)\n\s+(\((list_display|id)\s\d+)((\n\s+\(\w+\s\d+\s\w+\))*)\))')
+patt = re.compile(r'\(list_display(\s\d+((\n\s+\(id\s\d+\s\w+\))*)\)\n\s+(\((list_display|id|tuple)\s\d+)((\n\s+\(\w+\s\d+\s\w+\))*)\))')
+bad_patt = re.compile(r'\(list_display(\s\d+((\n\s+\(id\s\d+\s\w+\))*)\)\n\s+(\((list_display|id|tuple)\s\d+)((\n\s+\(\w+\s\d+\s\w+\))*)\))')
+binop = r'\(assign\s\d+\n\s+\(binop'
 def process(infile):
-    with open(infile, 'r') as f:
-        result = patt.sub(r'(target_list \1', f.read())
+    f = open(infile, 'r')
+    dump = f.read()
+    if re.search(binop, dump):
+        exit(1)
+    result = patt.sub(r'(target_list \1', dump)
+
+    f.close()
     return result
 
 
 if __name__ == '__main__':
     import sys
     arg = sys.argv[1]
-    with open(arg.split('.')[0] + '.ast.processed', 'w') as f:
-        f.write(process(arg))
+    result = process(arg)
+    with open(arg, 'w') as f:
+        f.write(result)
