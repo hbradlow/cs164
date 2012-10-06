193e0f26412e359fc16cb78eb0136c733ae80763
Kevin Lindkvist <kevinlindkvist@berkeley.edu>
2012-10-06 05:23:08 -0700

    target lists

--- src/stmts.cc
+++ src/stmts.cc
@@ -40,3 +40,75 @@ public:
 
 NODE_FACTORY (Println_AST, PRINTLN);
 
+class Tuple_AST : public AST_Tree {
+private:
+    bool isTarget = 0;
+    void convert_to_target()
+    {
+        isTarget = 1;
+    }
+    void print (ostream& out, int indent) {
+        string nm = "tuple";
+        if (isTarget)
+           nm = string("target_list");
+  
+        out << "(" << nm << " " << lineNumber ();
+        for_each_child (c, this) {
+            out << endl << setw (indent + 4) << "";
+            c->print (out, indent + 4);
+        } end_for;
+        out << ")";
+    }
+
+    NODE_CONSTRUCTORS(Tuple_AST, AST_Tree);
+};
+NODE_FACTORY(Tuple_AST, TUPLE);
+
+class List_AST : public AST_Tree {
+private:
+    bool isTarget = 0;
+    void convert_to_target()
+    {
+        isTarget = 1;
+    }
+    void print (ostream& out, int indent) {
+        string nm = "list_display";
+        if (isTarget)
+           nm = string("target_list");
+  
+        out << "(" << nm << " " << lineNumber ();
+        for_each_child (c, this) {
+            out << endl << setw (indent + 4) << "";
+            c->print (out, indent + 4);
+        } end_for;
+        out << ")";
+    }
+
+    NODE_CONSTRUCTORS(List_AST, AST_Tree);
+};
+NODE_FACTORY(List_AST, LIST);
+
+class Assignment_AST : public AST_Tree {
+private:
+    void print (ostream& out, int indent) {
+        string nm = "assignment";
+  
+        out << "(" << nm << " " << lineNumber ();
+        for_each_child (c, this) {
+            out << endl << setw (indent + 4) << "";
+            c->print (out, indent + 4);
+        } end_for;
+        out << ")";
+    }
+   Assignment_AST* post_make(){
+        for_each_child (c, this) {
+            if (c_i_ == 0)
+                c->convert_to_target();
+        } end_for;
+        return this; 
+   } 
+    NODE_CONSTRUCTORS(Assignment_AST, AST_Tree);
+};
+NODE_FACTORY(Assignment_AST,ASSIGN);
+
+
