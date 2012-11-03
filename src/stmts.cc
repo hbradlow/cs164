94a60a5e789e467cefd4204dc046a8b02dd34001
Henry_Weston_Bradlow <cs164-am@cory.EECS.Berkeley.EDU>
2012-11-03 13:43:51 -0700

    First init

---
+++ src/stmts.cc
@@ -0,0 +1,47 @@
+/* -*- mode: C++; c-file-style: "stroustrup"; indent-tabs-mode: nil; -*- */
+
+/* stmts.cc: AST subclasses related to statements and modules. */
+
+/* Authors:  YOUR NAMES HERE */
+
+#include <iostream>
+#include "apyc.h"
+#include "ast.h"
+#include "apyc-parser.hh"
+
+using namespace std;
+
+/*****   PRINTLN   *****/
+
+/** A print statement without trailing comma. */
+class Println_AST : public AST_Tree {
+protected:
+
+    NODE_CONSTRUCTORS (Println_AST, AST_Tree);
+
+    const char* externalName () {
+	return "println";
+    }
+
+};
+
+NODE_FACTORY (Println_AST, PRINTLN);
+
+/***** STMT_LIST *****/
+
+/** A list of statements. */
+class StmtList_AST : public AST_Tree {
+protected:
+
+    NODE_CONSTRUCTORS (StmtList_AST, AST_Tree);
+
+    AST_Ptr doOuterSemantics () {
+        for_each_child_var (c, this) {
+            c = c->doOuterSemantics ();
+        } end_for;
+        return this;
+    }
+
+};
+
+NODE_FACTORY (StmtList_AST, STMT_LIST);
