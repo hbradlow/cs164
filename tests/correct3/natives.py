f756482c6f47b6dca16891743748d3b3c88f2bde
Kevin Lindkvist <kevinlindkvist@berkeley.edu>
2012-12-03 23:29:21 -0800

    A lot of the prelude done

---
+++ tests/correct3/natives.py
@@ -0,0 +1,24 @@
+print 3 + 5
+print 5 - 7
+print 5 * 8
+print 10 / 2
+print 5 % 3
+print 5**2
+print -10
+print 5 < 10
+print 5 > 10
+print 4 <= 8
+print 4 >= 8
+print 5 == 10
+print 5 != 5
+print "add" + "string"
+print 3 * "hello"
+print "hello" * 3
+print "a" < "b"
+print "a" > "b"
+print "abc" <= "bcd"
+print "abc" >= "bcd"
+print "ab" == "ab"
+print "ab" != "ab"
+print "abc"[1]
+print "abc"[0:1]
