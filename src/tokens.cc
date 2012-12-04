a7b3b3d5a852bc70a7f47210162c581cf3b5dea9
Henry Bradlow <hbradlow@berkeley.edu>
2012-12-03 20:05:30 -0800

    Fixed call_if

--- src/tokens.cc
+++ src/tokens.cc
@@ -113,7 +113,7 @@ protected:
 
     //hbradlow
     void innerCodeGen(ostream& out,int i){
-        out << "Integer(" << value << ")";
+        out << "new Integer(" << value << ")";
     }
     //hbradlow
     void outerCodeGen(ostream& out,int i){
@@ -412,7 +412,7 @@ private:
 
     //hbradlow
     void innerCodeGen(ostream& out,int i){
-        out << "String(\"" << string_text() << "\")";
+        out << "new String(\"" << string_text() << "\")";
     }
     //hbradlow
     void outerCodeGen(ostream& out,int i){
